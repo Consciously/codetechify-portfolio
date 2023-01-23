5ae5ebdf683e531fa45c76862e6112a1ab89238f
Stefan Ihle <stefanihle@gmx.net>
2023-01-23 20:14:10 +0100

    change to next.js 13 beta add tailwindcss

---
+++ app/head.tsx
@@ -0,0 +1,18 @@
+const Head = () => {
+	return (
+		<>
+			<title>Contechify Website</title>
+			<meta name='viewport' content='width=device-width, initial-scale=1' />
+
+			{/* <link
+				rel='stylesheet'
+				href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css'
+				integrity='sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=='
+				crossOrigin='anonymous'
+				referrerPolicy='no-referrer'
+			/> */}
+		</>
+	);
+};
+
+export default Head;
